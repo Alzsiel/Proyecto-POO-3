1620ca56b33ed49a7df47130701488e0ed3cd94a
Alzsiel <65980872+Alzsiel@users.noreply.github.com>
2026-06-16 17:41:39 -0400

    Update Conductor.java

--- Conductor.java
+++ Conductor.java
@@ -3,6 +3,8 @@
 package modelo;
 
 import utilidades.*;
+import java.io.Serializable;
+
 public class Conductor extends Tripulante implements Serializable{
 
     public Conductor(IdPersona idPersona, Nombre nombre, Direccion direccion) {
