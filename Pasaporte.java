c92145affdb838a2049caf087e8b16c02b44e4a1
doliboy1923-Alias-burroPixel <tomas.meza2501@alumnos.ubiobio.cl>
2026-06-16 17:40:39 -0400

    Update Pasaporte.java

--- Pasaporte.java
+++ Pasaporte.java
@@ -1,6 +1,8 @@
 package utilidades;
 
-public class Pasaporte implements IdPersona {
+import java.io.Serializable;
+
+public class Pasaporte implements IdPersona, Serializable {
     private String numero;
     private String nacionalidad;
 
