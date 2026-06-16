60b91fecd2154d081884f75262e8532f7f5c82b1
doliboy1923-Alias-burroPixel <tomas.meza2501@alumnos.ubiobio.cl>
2026-06-16 17:36:23 -0400

    Delete Conductor.java

--- Conductor.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-import utilidades.*;
-public class Conductor extends Tripulante {
-
-    public Conductor(IdPersona idPersona, Nombre nombre, Direccion direccion) {
-        super(idPersona, nombre, direccion);
-    }
-}
