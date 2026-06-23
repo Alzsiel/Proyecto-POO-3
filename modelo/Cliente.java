6714ec2ce3b29dd23420b725f93c1fdcd102e332
marisolyb <marisol.yanez2501@alumnos.ubiobio.cl>
2026-06-22 22:50:46 -0400

    Add files via upload

---
+++ modelo/Cliente.java
@@ -0,0 +1,32 @@
+//Marisol Yañez Borquez
+package modelo;
+import java.util.*;
+import utilidades.*;
+import utilidades.Nombre;
+import java.io.Serializable;
+public class Cliente extends Persona implements Serializable{
+    private String email;
+    private ArrayList<Venta> ventas;
+
+    public Cliente(IdPersona idPersona, Nombre nombre, String email) {
+        super(idPersona, nombre);
+        this.email = email;
+        this.ventas = new ArrayList<Venta>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void addVenta(Venta venta) {
+        ventas.add(venta);
+    }
+
+    public Venta[] getVentas() {
+        return ventas.toArray(new Venta[0]);
+    }
+}
